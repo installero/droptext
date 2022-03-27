eba98907b920e6b918f0ba72bd274e2ad0962151
Vadim Venediktov <install.vv@gmail.com>
2022-03-27 18:05:10 +0300

    Puma production config

---
+++ config/puma/production.rb
@@ -0,0 +1,18 @@
+RAILS_ENV="production"
+APP_PATH = "/home/droptext/www"
+
+environment RAILS_ENV
+
+bind  "unix://#{APP_PATH}/puma.sock"
+pidfile "#{APP_PATH}/puma.pid"
+state_path "#{APP_PATH}/puma.state"
+directory "#{APP_PATH}/"
+
+stdout_redirect "#{APP_PATH}/log/puma.stdout.log", "#{APP_PATH}/log/puma.stderr.log", true
+
+workers 2
+threads 1,2
+
+activate_control_app "unix://#{APP_PATH}/pumactl.sock"
+
+prune_bundler
