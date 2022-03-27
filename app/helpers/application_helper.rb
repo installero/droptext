9440314ea975995b9d655d35748d878ca9ca130f
Vadim Venediktov <install.vv@gmail.com>
2022-03-27 20:17:00 +0300

    Open Graph meta tags added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,19 @@
 module ApplicationHelper
+  OG_META_TAGS = {
+    title: 'Droptext — закинь текст, отправвь ссылку',
+    site_name: 'Droptext',
+    description: 'Площадка для публикации снипетов'
+  }
+
   def text_for_display(text)
     h(text).gsub("\r\n", '<br>').html_safe
+  end
+
+  def og_meta_tags
+    tags = OG_META_TAGS.map do |property, content|
+      content_tag('meta', '', property: "og:#{property}", content: content)
+    end
+
+    tags.join("\n").html_safe
   end
 end
