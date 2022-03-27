863fe66da0cbe4f56058820bed75902214beebcd
Vadim Venediktov <install.vv@gmail.com>
2022-03-27 19:24:50 +0300

    About page added (via seed.rb)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+readme_path = Rails.root.join('README.md')
+
+content = File.read(readme_path)
+
+Text.create(body: content).update(token: 'about')
