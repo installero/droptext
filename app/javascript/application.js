e17250f4585211aa3352ecbc53bdf6f12692c135
Vadim Venediktov <install.vv@gmail.com>
2022-03-29 01:09:54 +0300

    Copy to clipboard button on snippet page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,14 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 import { HighlightJS } from "highlight.js"
+import ClipboardJS from "clipboard"
 
 document.addEventListener('turbo:load', (event) => {
   HighlightJS.highlightAll();
+
+  new ClipboardJS('.js-clipboard', {
+    text: function(trigger) {
+      return trigger.getAttribute('data-clipboard-text')
+    }
+  })
 })
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "highlight.js", to: "https://ga.jspm.io/npm:highlight.js@11.5.0/es/index.js", preload: true
+pin "clipboard", to: "https://ga.jspm.io/npm:clipboard@2.0.10/dist/clipboard.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
