02742f952bd4a03cf5acbd8203e68a5500ebc88e
Edem Topuzov <keltikan@gmail.com>
2022-03-29 15:36:44 +0300

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'snippets#new'
 
-  get '/text/:token', to: redirect('/s/%{token}')
+  get '/texts/:token', to: redirect('/s/%{token}')
   resources :s, only: %i[create show], param: :token, controller: :snippets, as: :snippets
 end
