3e6d95ed8170627c85cbbf2db5b7b278588fd166
Edem Topuzov <keltikan@gmail.com>
2022-03-29 15:22:23 +0300

    Change /t/ to /s/ path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'snippets#new'
 
-  get '/text/:token', to: redirect('/t/%{token}')
-  resources :t, only: %i[create show], param: :token, controller: :snippets, as: :snippets
+  get '/text/:token', to: redirect('/s/%{token}')
+  resources :s, only: %i[create show], param: :token, controller: :snippets, as: :snippets
 end
