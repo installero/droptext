b7002fd12a4dfbbb09d033b64b4dab8efba51546
Edem Topuzov <keltikan@gmail.com>
2022-04-01 22:06:38 +0300

    Switch from importmap to webpack

--- config/importmap.rb
+++
@@ -1,9 +0,0 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "highlight.js", to: "https://ga.jspm.io/npm:highlight.js@11.5.0/es/index.js", preload: true
-pin "clipboard", to: "https://ga.jspm.io/npm:clipboard@2.0.10/dist/clipboard.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ git pull &&
 bundle install &&
 RAILS_ENV=production bundle exec rails db:migrate &&
 RAILS_ENV=production bundle exec rails assets:precompile &&
+yarn build &&
 bundle exec whenever --update-crontab &&
 touch tmp/restart.txt
