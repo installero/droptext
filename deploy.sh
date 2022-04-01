de75a6b23f2203bac6e8f0a97511aa0225fd52c7
Edem Topuzov <keltikan@gmail.com>
2022-04-02 02:14:41 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ git pull &&
 bundle install &&
 RAILS_ENV=production bundle exec rails db:migrate &&
 RAILS_ENV=production bundle exec rails assets:precompile &&
+yarn install &&
 yarn build &&
 bundle exec whenever --update-crontab &&
 touch tmp/restart.txt
