04b52a7ca392df5a29a1d63ffd4e0e2883a2429a
Edem Topuzov <keltikan@gmail.com>
2022-04-10 15:07:56 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
-pages = {
-  'README.md' => 'about',
-  'disclamer.md' => 'disclamer'
-}
+pages =
+  [
+    { filename: 'README.md', markdown: :markdown, token: 'about' },
+    { filename: 'disclamer.md', markdown: :markdown, token: 'disclamer' }
+  ]
 
-pages.each do |filename, token|
+pages.each do |page|
+  page => { filename:, markdown:, token: }
   file_path = Rails.root.join(filename)
   content = File.read(file_path)
   snippet = Snippet.find_or_create_by!(token: token)
-  snippet.update(body: content)
+  snippet.update(body: content, markdown: markdown)
 end
