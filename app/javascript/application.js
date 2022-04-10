63194b5dee4ad0d692d21c4b082ad6c091439935
Edem Topuzov <keltikan@gmail.com>
2022-04-10 15:06:47 +0300

    Use choices.js autocomplete to select snippet markdown

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import './controllers'
 
 import hljs from './lib/highlight.js'
 import ClipboardJS from 'clipboard'
+import Choices from 'choices.js'
 
 document.addEventListener('turbo:load', (event) => {
   hljs.highlightAll()
@@ -12,4 +13,14 @@ document.addEventListener('turbo:load', (event) => {
       return trigger.getAttribute('data-clipboard-text')
     }
   })
+
+  const markdownSelect = document.querySelector('#snippet_markdown')
+
+  if (markdownSelect) {
+    new Choices(markdownSelect, {
+      noResultsText: markdownSelect.dataset.noResults,
+      itemSelectText: markdownSelect.dataset.itemSelect,
+      sorter: (a, b) => new Intl.Collator(markdownSelect.dataset.locale).compare(a.label, b.label),
+    })
+  }
 })
